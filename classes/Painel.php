121745a77987c01a4194707e11da791a4aed86f1
Luiz Serão <luiz.serao@hotmail.com>
2024-07-15 06:12:54 -0300

    primeiro commit

---
+++ classes/Painel.php
@@ -0,0 +1,19 @@
+<?php
+
+class Painel {
+  // Verifica se está logado
+  public static function isLogin() {
+    return isset($_SESSION['login']) ? true : false;
+  }
+
+   // Mensagem indicativa de sucesso ou erro
+   public static function alert($type, $message) {
+    if ($type === 'sucesso') {
+      echo '<p class="sucesso">'.$message.'</p>';
+    } else if ($type === 'erro') {
+      echo '<p class="erro">'.$message.'</p>';
+    }
+  }
+}
+
+?>
